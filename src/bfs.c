5a0e3b29398a367381352fe4817af8369cc75e51
lamachan <95586117+lamachan@users.noreply.github.com>
2022-04-07 11:33:38 +0200

    Prepared bfs.c and dijkstry.c for work!

---
+++ src/bfs.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*typedef struct fifo_element
+{
+        int vertex;
+        struct fifo_element *next;
+} fifo_element_t;
+
+typedef struct fifo
+{
+        int count;
+        fifo_element_t *front;
+        fifo_element_t *back;
+} * fifo_t;
+
+typedef enum colour {white, gray, black} colour_t;*/
+
+fifo_t initialise_fifo()
+{
+
+}
+
+void push_fifo(fifo_t q, int vertex)
+{
+
+}
+
+int pop_fifo(fifo_t q)
+{
+
+}
+
+int is_empty_fifo(fifo_t q)
+{
+
+}
+
+void print_fifo(fifo_t q)
+{
+
+}
+
+void free_fifo(fifo_t q)
+{
+
+}
+
+colour_t bfs(graph_t g, int start_vertex)
+{
+
+}
+
+int check_connectivity(graph_t g)
+{
+
+}
+
---
+++ src/dijkstry.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*typedef struct pq
+{
+        int *heap;
+        int count;
+        double *distance;
+        int *position;
+} * pq_t;
+
+typedef struct path
+{
+        int *predecessor;
+        double *distance;
+} * path_t;*/
+
+pq_t initialise_pq(int size)
+{
+
+}
+
+static void heap_up(pq_t q, int child)
+{
+
+}
+
+static void heap_down(pq_t q)
+{
+
+}
+
+void push_pq(pq_t q, int vertex, double distance)
+{
+
+}
+
+int pop_pq(pq_t q)
+{
+
+}
+
+int is_empty_pq(pq_t q)
+{
+
+}
+
+void print_pq(pq_t q)
+{
+
+}
+
+void free_pq(pq_t q)
+{
+
+}
+
+path_t dijkstry(graph_t g, int start_vertex)
+{
+
+}
+
+int find_path(graph_t g, int start_vertex, int finish_vertex)
+{
+
+}
+
