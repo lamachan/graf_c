1a0eeb8f1f0d016d8eeceb876dcbb7086473674f
lamachan <95586117+lamachan@users.noreply.github.com>
2022-04-11 22:02:47 +0200

    Write test

--- src/test_graph.c
+++ src/test_graph.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
 {
 	//random graph generation test
 	int rows, columns;
-	FILE * in = fopen(argv[1], "r");
+	/*FILE * in = fopen(argv[1], "r");
 	if(in == NULL)
 	{
 		printf("Blad otwierania!\n");
@@ -16,15 +16,19 @@ int main(int argc, char **argv)
         {
                 printf("Zły format rows, columns!\n");
 		return 1;
-        }
+        }*/
+	rows = 3;
+	columns = 4;
 	graph_t g = initialise_graph(rows, columns);
-	int x = read_graph(g, in);
-	printf("%d\n", x);
+	
+	/*int x = read_graph(g, in);
+	printf("%d\n", x);*/
 
-	//generate_graph(g, 1, 10);
+	generate_graph(g, 1, 10);
 #ifdef DEBUG
 	print_graph(g);
 #endif
+	write_graph(g, argv[1]);
 	//dijsktry test
 	//find_path(g, 0, 567822);
 
